d24afe25af93088dd70118ea60e9a206f82b7b27
Santiago <santiagomorales333@yahoo.com>
2024-03-15 12:51:32 -0700

    implemented dice game without procedural. utilized OOP approach

---
+++ DiceGame/Dice.cs
@@ -0,0 +1,14 @@
+﻿namespace DiceGame
+{
+    public class Dice
+    {
+        public static int Face { get; } = new Random().Next(1, 7);
+        
+
+    }
+    public static class DiceValidator
+    {
+        public static bool Correct(int guess) =>  guess == Dice.Face;
+        
+    }
+}
---
+++ DiceGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using DiceGame;
+var dice = new Dice();
+var user = new User();
+int maxTries = 3;
+
+Console.WriteLine($"Dice rolled. Guess what number it is in {maxTries} tries.");
+do
+{
+    int guess = user.GetInput();
+    if (DiceValidator.Correct(guess))
+    {
+        break;
+    }
+    else
+    {
+        maxTries--;
+        Console.WriteLine("Wrong number.");
+    }
+} while (maxTries > 0);
+Console.WriteLine(maxTries > 0 ? "You win!" : "You lose :(");
