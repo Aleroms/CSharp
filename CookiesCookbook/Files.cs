1be1fd7e21fc4fb21d5579d25cdbe935f6276069
Santiago <santiagomorales333@yahoo.com>
2024-03-22 15:05:30 -0700

    my implementation of cookies recipe

---
+++ CookiesCookbook/Files.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using CookiesCookbook;
+namespace CookieFiles
+{
+    public enum FileFormat { JSON, TXT };
+
+    public static class Files
+    {
+        public static void Save(List<Ingredient> payload, FileFormat format)
+        {
+            if (FileFormat.JSON == format)
+                SaveAsJson(payload);
+            else if (FileFormat.TXT == format)
+                SaveAsText(payload);
+
+        }
+        public static List<Ingredient> Read(FileFormat format)
+        {
+            return FileFormat.JSON == format ? ReadAsJson() : ReadAsText();
+        }
+
+        private static List<Ingredient> ReadAsText()
+        {
+            var temp = File.ReadAllText("recipies.text").Split(Environment.NewLine);
+            var result = new List<Ingredient>();
+            foreach (var line in temp)
+            {
+                if (int.TryParse(line, out var id))
+                {
+                    result.Add(IdToItem.GetIngredient((IngredientType)id));
+                }
+            }
+            Console.WriteLine(temp);
+            return result;
+        }
+
+        private static List<Ingredient> ReadAsJson()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SaveAsJson(List<Ingredient> payload)
+        {
+            var path = "recipies.json";
+            var allIds = new List<string>();
+            foreach (var ingredient in payload)
+            {
+                allIds.Add(ingredient.Id.ToString());
+            }
+
+            File.AppendAllText(path, JsonSerializer.Serialize(allIds));
+
+        }
+        private static void SaveAsText(List<Ingredient> payload)
+        {
+            var path = "recipies.text";
+            var allIds = new List<int>();
+            foreach (var ingredient in payload)
+            {
+                allIds.Add((int)ingredient.Id);
+            }
+            string recipiesAsString = string.Join(",", allIds);
+            Console.WriteLine(recipiesAsString);
+
+            File.AppendAllText(path, recipiesAsString + Environment.NewLine);
+        }
+    }
+}
+
+/*
+ * 
+                    var asJson = JsonSerializer.Serialize(payload);
+                    Console.WriteLine(asJson);
+
+                    File.WriteAllText(path, asJson);
+  */
---
+++ CookiesCookbook/Ingredient.cs
@@ -0,0 +1,128 @@
+﻿namespace CookiesCookbook
+{
+    public enum IngredientType
+    {
+        WHEAT, COCONUT, BUTTER, CHOCOLATE,
+        SUGAR, CARDAMOM, CINNAMON, COCOA
+    }
+    public static class IdToItem
+    {
+        public static Ingredient GetIngredient(IngredientType id)
+        {
+            
+            switch(id)
+            {
+                case IngredientType.WHEAT:
+                    return new Wheat();
+                case IngredientType.COCONUT:
+                    return new Coconut();
+                case IngredientType.BUTTER:
+                    return new Butter();
+                case IngredientType.SUGAR:
+                    return new Sugar();
+                case IngredientType.CHOCOLATE:
+                    return new Chocolate();
+                case IngredientType.CARDAMOM:
+                    return new Cardamom();
+                case IngredientType.CINNAMON:
+                    return new Cinnamon();
+                case IngredientType.COCOA:
+                    return new Cocoa();
+            }
+
+            return new Ingredient();
+        }
+    }
+    public class Ingredient
+    {
+        public IngredientType Id { get; init; }
+        public string Name { get; init; }
+
+        public Ingredient()
+        {
+            Id = 0;
+            Name = "defalt";
+        }
+        public Ingredient(IngredientType id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public override string ToString() => "Ingredient";
+        
+    }
+    public interface IRecipePreparation
+    {
+        string Prepare();
+    }
+
+    public class Wheat : Ingredient, IRecipePreparation
+    {
+        public Wheat() : base(IngredientType.WHEAT, "Wheat flour")
+        {
+        }
+        public string Prepare() => "Sieve. Add to other ingredients";
+        public override string ToString() => Name;
+    }
+    public class Coconut : Ingredient, IRecipePreparation
+    {
+        public Coconut() : base(IngredientType.COCONUT, "Coconut flour")
+        {
+        }
+        public string Prepare() => "Sieve. Add to other ingredients.";
+        public override string ToString() => Name;
+    }
+    public class Butter : Ingredient, IRecipePreparation
+    {
+        public Butter() : base(IngredientType.BUTTER, "Butter")
+        {
+        }
+        public string Prepare() => "Melt on low heat. Add to other ingredients.";
+        public override string ToString() => Name;
+    }
+    public class Chocolate : Ingredient, IRecipePreparation
+    {
+        public Chocolate() : base(IngredientType.CHOCOLATE, "Chocolate")
+        {
+        }
+        public string Prepare() => "Melt in a water bath. Add to other ingredients.";
+        public override string ToString() => Name;
+    }
+    public class Sugar : Ingredient, IRecipePreparation
+    {
+        public Sugar() : base(IngredientType.SUGAR, "Sugar")
+        {
+        }
+
+        public string Prepare() => "Add to other ingredients.";
+        public override string ToString() => Name;
+    }
+    public class Cardamom : Ingredient, IRecipePreparation
+    {
+        public Cardamom() : base(IngredientType.CARDAMOM, "Cardamom")
+        {
+        }
+
+        public string Prepare() => "Take half a teaspoon. Add to other ingredients.";
+        public override string ToString() => Name;
+    }
+    public class Cinnamon : Ingredient, IRecipePreparation
+    {
+        public Cinnamon() : base(IngredientType.CINNAMON, "Cinamon")
+        {
+        }
+
+        public string Prepare() => "Take half a teaspoon. Add to other ingredients.";
+        public override string ToString() => Name;
+    }
+    public class Cocoa : Ingredient, IRecipePreparation
+    {
+        public Cocoa() : base(IngredientType.COCOA, "Cocoa powder")
+        {
+        }
+
+        public string Prepare() => "Add to other ingredients";
+        public override string ToString() => Name;
+    }
+
+}
