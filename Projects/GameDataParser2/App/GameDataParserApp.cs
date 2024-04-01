57ba9aac387d2e2c2a1a3887f682e07981c46d2a
Santiago <santiagomorales333@yahoo.com>
2024-04-01 13:03:08 -0700

    reorganized repository structure

---
+++ Projects/GameDataParser2/App/GameDataParserApp.cs
@@ -0,0 +1,30 @@
+﻿public class GameDataParserApp
+{
+    private readonly IUserInteractor _userInteractor;
+    private readonly IGamesPrinter _gamesPrinter;
+    private readonly IVideoGamesDeserializer _videoGamesDeserializer;
+    private readonly IFileReader _reader;
+
+    public GameDataParserApp(
+        IGamesPrinter gamesPrinter,
+        IUserInteractor userInteractor,
+        IVideoGamesDeserializer videoGamesDeserializer,
+        IFileReader reader)
+    {
+        _gamesPrinter = gamesPrinter;
+        _userInteractor = userInteractor;
+        _videoGamesDeserializer = videoGamesDeserializer;
+        _reader = reader;
+    }
+
+
+    public void Run()
+    {
+        string fileName = _userInteractor.ReadValidFilePath();
+        var fileContents = _reader.Read(fileName);
+        var videoGames = _videoGamesDeserializer.DeserializeFrom(
+            fileName, fileContents);
+        _gamesPrinter.Print(videoGames);
+    }
+
+}
---
+++ Projects/GameDataParser2/Logging/Logger.cs
@@ -0,0 +1,19 @@
+﻿public class Logger
+{
+    private readonly string _logFileName;
+
+    public Logger(string v)
+    {
+        _logFileName = v;
+    }
+    public void Log(Exception e)
+    {
+        var entry =
+$@"[{DateTime.Now}]
+Exception message: {e.Message}
+Stack trace: {e.StackTrace}
+
+";
+        File.AppendAllText(_logFileName, entry);
+    }
+}
