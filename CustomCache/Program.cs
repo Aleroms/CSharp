5188bf648fb0e0ced4dc20621c1a727785bb6d2e
Santiago <santiagomorales333@yahoo.com>
2024-04-03 18:05:13 -0700

    custom caching project that simulates API fetching.

---
+++ CustomCache/Program.cs
@@ -0,0 +1,53 @@
+﻿IDataDownloader dataDownloader = new CacheDataDownloader(
+    new SlowDataDownloader());
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.ReadKey();
+
+public interface IDataDownloader
+{
+    string DownloadData(string resourceId);
+}
+public class CacheDataDownloader : IDataDownloader
+{
+    private readonly Cache<string, string> _cache = new();
+    private readonly IDataDownloader _dataDownloader;
+    public CacheDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+public class SlowDataDownloader : IDataDownloader
+{
+    public string DownloadData(string resourceId)
+    {
+        //let's imagine this method downloads real data,
+        //and it does it slowly
+        Thread.Sleep(1000);
+        return $"Some data for {resourceId}";
+    }
+}
+public class Cache<TKey, TData>
+{
+    private readonly Dictionary<TKey, TData> _cachedData = new();
+
+    public TData Get(TKey key, Func<TKey, TData> getForFirstTime)
+    {
+        if (!_cachedData.ContainsKey(key))
+            _cachedData[key] = getForFirstTime(key);
+
+        return _cachedData[key];
+    }
+}
